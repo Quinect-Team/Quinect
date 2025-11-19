18f5faf7cf05485ba73efea37f59d41c17437263
m1suyeon <xljzxc11@gmail.com>
2025-11-19 18:06:26 +0900

    문제 유형 선택 시 서식 변경
    templates.layout.quizlist: 퀴즈 리스트 형식만 잡음(그리드)
    templates.layout.setquestion: 문제 출제 하는 페이지(객관식, 서술형 스왑 가능)
    QuizController: 혹시 몰라서 컨트롤러 따로 빼둠

---
+++ src/main/java/com/project/quiz/controller/QuizController.java
@@ -0,0 +1,18 @@
+package com.project.quiz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class QuizController {
+	@GetMapping("/list")
+    public String list() {
+        return "/layout/quiz_list";  // src/main/resources/templates/layout/quiz_list.html을 렌더링
+    }
+	
+	@GetMapping("/setquestion")
+    public String setquestion() {
+        return "/layout/setquestion";  // src/main/resources/templates/layout/setquestion.html을 렌더링
+    }
+
+}
