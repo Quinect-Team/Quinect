30dac8f935c9ded4e87b474561d5429cbb1e18f3
hyeongyumin949 <hyeongyumin949@gmail.com>
2025-11-20 12:17:46 +0900

    1120
    버그수정

--- src/main/java/com/project/quiz/config/QRCodeService.java
+++ src/main/java/com/project/quiz/config/QRCodeService.java
@@ -1,17 +1,18 @@
 package com.project.quiz.config;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import org.springframework.stereotype.Service;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class QRCodeService {
