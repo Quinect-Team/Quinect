0a65db60fc6c88b3e524de2756ef0449cf7cb42e
hyeongyumin949 <hyeongyumin949@gmail.com>
2025-11-25 12:09:25 +0900

    사이드바, 상점 모달 정상화
    123

--- src/main/resources/static/js/sb-admin-2.js
+++ src/main/resources/static/js/sb-admin-2.js
@@ -2,24 +2,52 @@
   "use strict"; // Start of use strict
   
   // 모달 열기 함수 (jQuery 버전)
-  function openSidebarModal() {
-    $('body').css('overflow', 'hidden'); // [translate:배경 스크롤 막기]
-    $('#sidebarModalBackdrop').css('display', 'block');
-    $('#sidebarModal').css('display', 'block');
-    setTimeout(function() {
-      $('#sidebarModal').addClass('show');
-    }, 10);
-  }
+  function getScrollbarWidth() {
+      return window.innerWidth - document.documentElement.clientWidth;
+    }
 
-  // 모달 닫기 함수 (jQuery 버전)
-  function closeSidebarModal() {
-    $('body').css('overflow', 'auto'); // [translate:배경 스크롤 허용]
-    $('#sidebarModal').removeClass('show');
-    setTimeout(function() {
-      $('#sidebarModal').css('display', 'none');
-      $('#sidebarModalBackdrop').css('display', 'none');
-    }, 300);
-  }
+    // 2. 모달 열기 함수
+    function openSidebarModal() {
+      // 스크롤바 너비 구하기
+      var scrollbarWidth = getScrollbarWidth();
+
+      // body에 스크롤바 너비만큼 패딩을 줘서 밀림 방지 + 스크롤 막기
+      $('body').css({
+          'overflow': 'hidden',
+          'padding-right': scrollbarWidth + 'px'
+      });
+      
+      // (선택사항) 만약 상단바(Navbar)가 고정(fixed)되어 있다면 걔도 밀어줘야 함
+      // $('.navbar-fixed-top').css('padding-right', scrollbarWidth + 'px');
+
+      // 기존 로직 유지 (슬라이드 효과)
+      $('#sidebarModalBackdrop').css('display', 'block');
+      $('#sidebarModal').css('display', 'block');
+      
+      setTimeout(function() {
+        $('#sidebarModal').addClass('show');
+      }, 10);
+    }
+
+    // 3. 모달 닫기 함수
+    function closeSidebarModal() {
+      // 먼저 슬라이드 들어가게 하기 (클래스 제거)
+      $('#sidebarModal').removeClass('show');
+      
+      // 애니메이션 시간(300ms) 뒤에 뒷정리
+      setTimeout(function() {
+        $('#sidebarModal').css('display', 'none');
+        $('#sidebarModalBackdrop').css('display', 'none');
+        
+        // 스크롤바 및 패딩 원상복구 (여기가 핵심!)
+        $('body').css({
+            'overflow': 'auto',
+            'padding-right': '0px'
+        });
+        // $('.navbar-fixed-top').css('padding-right', '0px');
+        
+      }, 300);
+    }
 
   // 페이지 로드 시 초기화 및 이벤트 바인딩
   $(document).ready(function() {
